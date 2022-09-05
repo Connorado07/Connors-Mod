6c536af95a2d4352dcaba63123e8710eae89044c
ronnoxtyb <ronnoxtyb@gmail.com>
2022-09-05 17:19:40 -0600

    Fixed models, names and mixin prints. Added other SS parts. Now Crashing...

--- src/main/java/io/connor/connors_mod/Star_Wars/items/StarWarsItems.java
+++ src/main/java/io/connor/connors_mod/Star_Wars/items/StarWarsItems.java
@@ -10,21 +10,28 @@ import net.minecraft.util.registry.Registry;
 
 public class StarWarsItems {
 
-
-    public static final Item KYBER_CRYSTAL_RED = registerItem("kyber_crystal_red",
+    //Kyber Crystals (Color Crystals)
+    public static final Item RED_KYBER_CRYSTAL = registerItem("red_kyber_crystal",
             new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).rarity(Rarity.RARE).maxCount(1).fireproof()));
-    public static final Item KYBER_CRYSTAL_BLUE = registerItem("kyber_crystal_blue",
+    public static final Item BLUE_KYBER_CRYSTAL = registerItem("blue_kyber_crystal",
             new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).rarity(Rarity.RARE).maxCount(1)));
-    public static final Item KYBER_CRYSTAL_GREEN = registerItem("kyber_crystal_green",
+    public static final Item GREEN_KYBER_CRYSTAL = registerItem("green_kyber_crystal",
             new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).rarity(Rarity.RARE).maxCount(1)));
-    public static final Item KYBER_CRYSTAL_PURPLE = registerItem("kyber_crystal_purple",
+    public static final Item PURPLE_KYBER_CRYSTAL = registerItem("purple_kyber_crystal",
             new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).rarity(Rarity.RARE).maxCount(1)));
-    public static final Item KYBER_CRYSTAL_YELLOW = registerItem("kyber_crystal_yellow",
+    public static final Item YELLOW_KYBER_CRYSTAL = registerItem("yellow_kyber_crystal",
             new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).rarity(Rarity.RARE).maxCount(1)));
-    public static final Item KYBER_CRYSTAL_WHITE = registerItem("kyber_crystal_white",
+    public static final Item WHITE_KYBER_CRYSTAL = registerItem("white_kyber_crystal",
             new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).rarity(Rarity.RARE).maxCount(1)));
 
-
+    //Materials
+    public static final Item DURASTEEL_INGOT = new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP));
+    public static final Item PLASTANIUM_INGOT = new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP));
+    public static final Item QUADERNIUM_INGOT = new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP));
+    public static final Item FOCUS_CRYSTAL = new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).maxCount(1));
+    public static final Item BOTTOM_SABER_PART = new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).maxCount(16).rarity(Rarity.UNCOMMON));
+    public static final Item TOP_SABER_PART = new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP).maxCount(16).rarity(Rarity.UNCOMMON));
+    public static final Item PLASTANIUM_BALL = new Item(new FabricItemSettings().group(ModItemGroups.SABERS_GROUP));
 
 
     private static Item registerItem(String name, Item item){
